0f2599c6df5698c832f2b62f143f485044164f37
Tushxrb <tusharbanjan001@gmail.com>
2025-03-25 09:01:07 +0530

    Table and data

--- libraryproject/app/views.py
+++ libraryproject/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import Book
+from datetime import datetime
 
 # Create your views here.
 
@@ -11,5 +12,12 @@ from .models import Book
 def index(req):
     allbooks = Book.objects.all()
     print(allbooks)
-    context = {'myname': "ITV"}
+    # context = {'myname': "ITV"}
+    # return render(req, "index.html", context)
+    myname = "Tushar"
+    print(datetime.now())
+    curdate = datetime.now()
+    hour = datetime.now().hour
+    print(hour)
+    context = {"myname": myname, "curdate":curdate, "hour":hour, "allbooks":allbooks}
     return render(req, "index.html", context)
