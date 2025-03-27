1e52b90849e0a980847cdceac7a1850c46a2a959
Tushxrb <tusharbanjan001@gmail.com>
2025-03-27 09:08:39 +0530

    validation

--- libraryproject/app/views.py
+++ libraryproject/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Book
 from datetime import datetime
 
@@ -21,3 +21,28 @@ def index(req):
     print(hour)
     context = {"myname": myname, "curdate":curdate, "hour":hour, "allbooks":allbooks}
     return render(req, "index.html", context)
+
+def signup(req):
+    print(req.method)
+    if req.method=="GET":
+        return render(req, 'signup.html')
+    else:
+        print(req.method)
+        uname=req.POST["uname"]
+        upass=req.POST["upass"]
+        ucpass=req.POST["ucpass"]
+        print(uname, upass, ucpass)
+        if upass != ucpass:
+            errmsg='Password and Confirm password must be same'
+            context={'errmsg':errmsg}
+            return render(req, 'signup.html', context)
+        else:
+            return redirect("signin")
+
+
+def signin(req):
+    print(req.method)
+    return render(req, 'signin.html')
+
+
+
