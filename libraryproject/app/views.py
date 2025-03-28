62d47c734734c2fd078c41da4ef131aae05a1c95
Tushxrb <tusharbanjan001@gmail.com>
2025-03-28 09:04:14 +0530

    username-password authenticate

--- libraryproject/app/views.py
+++ libraryproject/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Book
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -29,20 +30,55 @@ def signup(req):
     else:
         print(req.method)
         uname=req.POST["uname"]
+        uemail=req.POST["uemail"]
         upass=req.POST["upass"]
         ucpass=req.POST["ucpass"]
-        print(uname, upass, ucpass)
+        print(uname, upass, ucpass,uemail)
         if upass != ucpass:
             errmsg='Password and Confirm password must be same'
             context={'errmsg':errmsg}
             return render(req, 'signup.html', context)
+        elif uname==upass:
+            errmsg='Email address and password must not be same'
+            context={'errmsg':errmsg}
+            return render(req, 'signup.html', context)
         else:
-            return redirect("signin")
+            try:
+                userdata = User.objects.create(username=uname, email=uemail, password=upass)
+                userdata.set_password(upass)
+                userdata.save()
+                print(User.objects.all())
+                return redirect("signin")
+            except:
+                errmsg="User already exists. Try with different username"
+                context = {"errmsg":errmsg}
+                return render(req, "signup.html", context)
 
+from django.contrib.auth import authenticate, login, logout
 
 def signin(req):
-    print(req.method)
-    return render(req, 'signin.html')
-
+    if req.method=="GET":
+        print(req.method)
+        return render(req, 'signin.html')
+    else:
+        uname = req.POST.get("uname")
+        uemail=req.POST.get('uemail')
+        upass=req.POST['upass']
+        print(uemail, upass)
+        # userdata=User.objects.filter(email=uemail, password=upass)
+        userdata = authenticate(username=uname, email=uemail, password=upass)
+        print(userdata)
+        if userdata is not None:
+            login(req, userdata)
+            # return render(req, 'dashboard.html')
+            return redirect("dashboard")
+        else:
+            context ={}
+            context['errmsg'] = 'Invalid email or password'
+            return render(req, "signin.html", context)
+        
+def dashboard(req):
+    print(req.user)
+    return render(req, "dashboard.html")
 
 
