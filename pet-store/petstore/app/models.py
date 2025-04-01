209d5526899ecf00e80331f33628e4e7af7a2ff9
Tushxrb <tusharbanjan001@gmail.com>
2025-04-01 08:58:28 +0530

    newproject

---
+++ pet-store/petstore/app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Pet(models.Model):
+    petid = models.PositiveIntegerField(primary_key=True)
+    petname = models.CharField(max_length=30, null=True, default=None)
+    userid = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)
+    age = models.PositiveIntegerField()
+    type = (('Male', 'Male'), ('Female', 'Female'))
+    gender = models.CharField(max_length=20, choices=type)
+    description = models.TextField()
+    photo = models.ImageField(upload_to="images")
---
+++ pet-store/petstore/app/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Pet
+from datetime import datetime
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def index(req):
+    allpets = Pet.objects.all()
+    return render(req, 'index.html',{"allpets":allpets})
+
+def signup(req):
+    print(req.method)
+    if req.method=="GET":
+        return render(req, 'signup.html')
+    else:
+        print(req.method)
+        uname=req.POST["uname"]
+        uemail=req.POST["uemail"]
+        upass=req.POST["upass"]
+        ucpass=req.POST["ucpass"]
+        print(uname, upass, ucpass,uemail)
+        if upass != ucpass:
+            errmsg='Password and Confirm password must be same'
+            context={'errmsg':errmsg}
+            return render(req, 'signup.html', context)
+        elif uname==upass:
+            errmsg='Email address and password must not be same'
+            context={'errmsg':errmsg}
+            return render(req, 'signup.html', context)
+        else:
+            try:
+                userdata = User.objects.create(username=uname, email=uemail, password=upass)
+                userdata.set_password(upass)
+                userdata.save()
+                print(User.objects.all())
+                return redirect("signin")
+            except:
+                errmsg="User already exists. Try with different username"
+                context = {"errmsg":errmsg}
+                return render(req, "signup.html", context)
+
+from django.contrib.auth import authenticate, login, logout
+
+def signin(req):
+    if req.method=="GET":
+        print(req.method)
+        return render(req, 'signin.html')
+    else:
+        uname = req.POST.get("uname")
+        uemail=req.POST.get('uemail')
+        upass=req.POST['upass']
+        print(uemail, upass)
+        # userdata=User.objects.filter(email=uemail, password=upass)
+        userdata = authenticate(username=uname, email=uemail, password=upass)
+        print(userdata)
+        if userdata is not None:
+            login(req, userdata)
+            # return render(req, 'dashboard.html')
+            return redirect("dashboard")
+        else:
+            context ={}
+            context['errmsg'] = 'Invalid email or password'
+            return render(req, "signin.html", context)
+        
+def dashboard(req):
+    print(req.user)
+    username= req.user
+    return render(req, "dashboard.html", {"username":username})
+
+def userlogout(req):
+    logout(req)
+    return redirect('/')
+
+
